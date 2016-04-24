cf56755eee3cb1c96e36387ba51a1c0e0809e6eb
ashrithhc <ashrithhcs@gmail.com>
2016-04-24 10:28:21 +0530

    Notifications basics

--- js/client/client.js
+++ js/client/client.js
@@ -8,27 +8,35 @@ socketio.on('disconnect', function(){
 
 socketio.on("userListInit", function(data){
     $('.myUsername').append('<p class="pull-right">'+whoAmI+'</p>');
-    $('.contacts').empty().append('<li class="contact_name selected_contact" value="All"><i class="glyphicon glyphicon-user"> </i> All</li>');
+    $('.contacts').empty().append('<li class="contact_name selected_contact" value="All"><i class="glyphicon glyphicon-user"> </i> All<i class="notify pull-right glyphicon glyphicon-envelope"> </i></li>');
     $('.chatContent').empty().append('<div class="All selected_content contact_content"></div>');
     $.each(data['userList'], function(index, uname){
         if(uname == whoAmI) return true;
-        $('.contacts').append('<li class="contact_name" value="'+uname+'"><i class="glyphicon glyphicon-user"></i> '+uname+'</li>');
+        $('.contacts').append('<li class="contact_name" value="'+uname+'"><i class="glyphicon glyphicon-user"></i> '+uname+'<i class="notify pull-right glyphicon glyphicon-envelope"> </i></li>');
         $('.chatContent').append('<div class="'+uname+' hide_content contact_content"></div>');
     });
 });
 
 socketio.on("message_to_client", function(data){
     if (data['fromWhom'] == whoAmI){
+        //The message was sent by this client
         var msgClass = 'messageRight';
         var msgDiv = data['toWhom'];
     }
-    else if (data['toWhom'] == 'All'){
-        var msgClass = 'messageLeft';
-        var msgDiv = data['toWhom'];
-    }    
     else {
-        var msgClass = 'messageLeft';
-        var msgDiv = data['fromWhom'];
+        if (data['toWhom'] == 'All'){
+            var msgClass = 'messageLeft';
+            var msgDiv = data['toWhom'];
+        }
+        else {
+            var msgClass = 'messageLeft';
+            var msgDiv = data['fromWhom'];
+        }
+        $('.contact_name').each(function(){
+            if( $(this).attr('value') == msgDiv ){
+                $(this).children('.notify').show();
+            }
+        });
     }
     $('.chatContent .'+msgDiv).html($('.chatContent .'+msgDiv).html() + "<hr/>" + "<p class='"+msgClass+"'><span class='unameChat'>"+data['fromWhom']+": "+"</span>" + "<span>"+data['message']+"</span></p>");
 });
@@ -43,7 +51,7 @@ socketio.on("usernameVerify", function(data){
 
 socketio.on("userListChange", function(data){
     if(data['action'] == 'add'){
-        $('.contacts').append('<li class="contact_name" value="'+data['username']+'"><i class="glyphicon glyphicon-user"></i> '+data['username']+'</li>');
+        $('.contacts').append('<li class="contact_name" value="'+data['username']+'"><i class="glyphicon glyphicon-user"></i> '+data['username']+'<i class="notify pull-right glyphicon glyphicon-envelope"> </i></li>');
         $('.chatContent').append('<div class="'+data['username']+' hide_content contact_content"></div>');
     }
     else if(data['action'] == 'delete'){
@@ -64,11 +72,12 @@ $(document).ready(function(){
     });
 
     $('body').on('click', '.contact_name', function(){
+        $(this).children('.notify').hide();
         toWhom = $(this).attr('value');
         $('.contact_name').removeClass('selected_contact');
         $(this).addClass('selected_contact');
         $('.contact_content').addClass('hide_content').removeClass('selected_content');
-        $('.chatContent .'+toWhom).addClass('selected_content').removeClass('hide_content').show();
+        $('.chatContent .'+toWhom).addClass('selected_content').removeClass('hide_content').show().scrollbar();
     });
 
     $('#sendButton').on('click', function(){
